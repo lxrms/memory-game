65ae8f92b2003f391a767da1071b87ef1daec222
Alex Ramos <oemaildoalexr@gmail.com>
2022-03-07 16:05:23 -0300

    initial commit'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import SingleCard from "./components/SingleCard";
+
+const cardImages = [
+  { src: "/img/react.png", matched: false },
+  { src: "/img/vue.png", matched: false },
+  { src: "/img/solid.png", matched: false },
+  { src: "/img/alpine.png", matched: false },
+  { src: "/img/angular.png", matched: false },
+  { src: "/img/svelte.png", matched: false },
+];
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort((a, b) => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true);
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000);
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  useEffect(() => shuffleCards(), []);
+
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setDisabled(false);
+    setTurns((prevTurns) => prevTurns + 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Jogo da Memória</h1>
+      <button onClick={shuffleCards}>Novo jogo</button>
+      <p>Rodada: {turns}</p>
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard
+            card={card}
+            disabled={disabled}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            handleChoice={handleChoice}
+            key={card.id}
+          />
+        ))}
+      </div>
     </div>
   );
 }
